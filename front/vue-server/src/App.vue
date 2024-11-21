1fa437f642802c20802e2798d527b44572be0518
ungit003 <ungit003@gmail.com>
2024-11-22 04:23:03 +0900

    241122 app view transition

--- front/vue-server/src/App.vue
+++ front/vue-server/src/App.vue
@@ -1,16 +1,55 @@
 <template>
-  <div>
-    <!-- <RouterLink :to="{ name: 'Start'}"></RouterLink>
-    <RouterLink :to="{ name: 'Recommend'}"></RouterLink> -->
-    <h1>Colors of Emotions</h1>
-    <RouterView />
+  <div class="container">
+    <Transition name="fade">
+      <h1 v-if="showTitle" class="title">Colors of Emotions</h1>
+    </Transition>
+    <Transition name="fade">
+      <RouterView v-if="showRouter" />
+    </Transition>
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
 
+const showTitle = ref(false);
+const showRouter = ref(false);
+
+onMounted(() => {
+  setTimeout(() => {
+    showTitle.value = true;
+  }, 1000);
+
+  setTimeout(() => {
+    showTitle.value = false;
+  }, 4000);
+
+  setTimeout(() => {
+    showRouter.value = true;
+  }, 5000);
+});
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
+.title {
+  font-size: 2rem;
+  text-align: center;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
