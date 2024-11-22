f1781066833d8e265641c218ae83906b759f0522
PoloCeleste <voidness12@gmail.com>
2024-11-23 00:12:55 +0900

    update views.actors

--- back/movies/views.py
+++ back/movies/views.py
@@ -19,6 +19,7 @@ class EmotionColorList(generics.ListAPIView):
 @api_view(['GET'])
 def Actors(request, movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
+    delp=['adult', 'gender', 'id', 'name', 'popularity', 'cast_id', 'credit_id', 'order']
     headers = {
         "accept": "application/json",
         "Authorization": f"Bearer {tmdb}"
@@ -26,6 +27,13 @@ def Actors(request, movie_id):
     try:
         response = requests.get(url, headers=headers).json()['cast']
         if response:
+            for re in response:
+                try:re['profile_path']='https://image.tmdb.org/t/p/original'+re['profile_path']
+                except:pass
+                for d in delp:
+                    try: del re[d]
+                    except: continue
+            
             data={
                 'cast':response
             }
