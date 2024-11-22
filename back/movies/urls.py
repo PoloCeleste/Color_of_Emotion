e375137be1882812bb5822c5438ad35b1318057a
PoloCeleste <voidness12@gmail.com>
2024-11-22 10:50:38 +0900

    update test urls

--- back/movies/urls.py
+++ back/movies/urls.py
@@ -1,6 +1,9 @@
 ﻿from django.urls import path
+from .views import MovieList, EmotionColorList
 
 app_name = 'movies'
 
 urlpatterns = [
+    path('movies/', MovieList.as_view(), name='movie-list'),
+    path('emotion-colors/', EmotionColorList.as_view(), name='emotion-color-list'),
 ]
--- back/movies/views.py
+++ back/movies/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Movie, EmotionColor
+from .serializers import MovieSerializer, EmotionColorSerializer
 
-# Create your views here.
+class MovieList(generics.ListAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class EmotionColorList(generics.ListAPIView):
+    queryset = EmotionColor.objects.all()
+    serializer_class = EmotionColorSerializer
